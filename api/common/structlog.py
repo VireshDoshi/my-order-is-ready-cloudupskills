8ebebbe805123849bbedaa02739137a70066dc74
Viresh Doshi <vireshdoshi@gmail.com>
2021-12-20 14:04:42 +0000

    lots of new stuff

--- api/common/structlog.py
+++ api/common/structlog.py
@@ -2,6 +2,7 @@ import structlog
 from structlog.dev import set_exc_info, ConsoleRenderer
 from structlog.processors import StackInfoRenderer, TimeStamper, add_log_level
 
+
 def config_structlog():
     structlog.configure_once(
         processors=[
